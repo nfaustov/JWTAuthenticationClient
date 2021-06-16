9d1cc60896cc699b2bb5cb622c9ea4b7a31f5400
Nikolai Faustov <61990876+nfaustov@users.noreply.github.com>
2021-06-16 18:17:57 +0300

    First commit

---
+++ JWTAuthenticationClient/Application/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  JWTAuthenticationClient
+//
+//  Created by Nikolai Faustov on 16.06.2021.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        let viewController = ViewController()
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+    }
+}
+
---
+++ JWTAuthenticationClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JWTAuthenticationClient
+//
+//  Created by Nikolai Faustov on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
